6cfe77b78ccc9cb8f6de9c9935de45b5206b8c57
luwat <oluwatobioreniyi@gmail.com>
2022-03-30 07:32:29 +0100

    practice 01 & 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let myString = '';
+    if (num < 0) {
+        myString = 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        myString += string;
+    }
+    return myString;
 };
+
+const number = Math.floor(Math.random() * 1000);
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', 3);
+repeatString('hey', -1);
+repeatString('hey', number);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
