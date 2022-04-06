56456a9fa55506e21f6b03e8d0c22ceee03c117d
luwat <oluwatobioreniyi@gmail.com>
2022-04-06 17:21:41 +0100

    assignment 3 and 4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    toLetters = word.split('');
+    reversedWord = toLetters.reverse().join('');
+    return reversedWord;
 };
 
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
