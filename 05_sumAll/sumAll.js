7382f42068fa4d61db0bdd22c9738a12d2657490
luwat <oluwatobioreniyi@gmail.com>
2022-04-11 08:56:09 +0100

    Assignment 5 and 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        sum = 'ERROR';
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        sum = 'ERROR';
+     } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) sum += i;
+    } else if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) sum += i;
+    } else if (num1 === num2) {
+        sum += num1 + num2;
+    } else {
+        sum = 'ERROR';
+    }
+    return sum;
 };
 
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(10, '90');
+sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leap = true;
+    let notLeap = false;
 
+    if (year%400 === 0) {
+        return leap;
+    } else if (year%4 === 0 && !(year%100 === 0)) {
+        return leap;
+    } else {
+        return notLeap
+    }
+    
 };
 
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
