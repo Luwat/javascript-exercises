d38aa4946aa148f5c22b8c99346a54fecc681312
luwat <oluwatobioreniyi@gmail.com>
2022-04-12 17:11:57 +0100

    assignment 07 done, 7 practices done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return Math.round((celcius * 9/5 + 32) * 10) / 10;
 };
 
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+ctof(0);
+ctof(73.2);
+ctof(-10);
 // Do not edit below this line
 module.exports = {
   ftoc,
