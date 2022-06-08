cbba717c7e5a5fcb8ebe016c71eff13ebdd92bed
luwat <oluwatobioreniyi@gmail.com>
2022-06-08 08:31:37 +0100

    done with calculator practice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,8 +24,15 @@ const power = function(num, pow) {
 	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return -1
+  } else if (num == 0 || num == 1) {
+    return 1
+  } else {
+    let result = num * factorial(num - 1);
+    return result;
+  }
 };
 
 
@@ -40,6 +47,11 @@ sum([1, 3, 5, 7, 9])
 multiply([2, 4])
 multiply([2, 4, 6, 8, 10, 12, 14])
 power(4, 3)
+factorial(0)
+factorial(1)
+factorial(2)
+factorial(5)
+factorial(10)
 // Do not edit below this line
 module.exports = {
   add,
