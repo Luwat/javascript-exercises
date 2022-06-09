1ffe96cfce269fe3a63bb0e7f1cca5980bb69ecc
luwat <oluwatobioreniyi@gmail.com>
2022-06-09 07:56:12 +0100

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const reg = /[\W_]/g;
+    let regLow = string.toLowerCase().replace(reg, '');
+    let regLowReverse = regLow.split('').reverse().join('');
+    return regLow === regLowReverse;
 
 };
+
+palindromes('racecar')
+palindromes('racecar!')
+palindromes('Racecar!')
+palindromes('Racecar!')
+palindromes('A car, a man, a maraca.')
+palindromes('Animal loots foliated detail of stool lamina.')
+palindromes('ZZZZ car, a man, a maracaz.')
 
 // Do not edit below this line
 module.exports = palindromes;
