4d96ccaf0f847afd7a9add2b7cae88e3fc3dc0dc
luwat <oluwatobioreniyi@gmail.com>
2022-06-10 07:46:57 +0100

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
+    if (num === 0) return 0;
+    if (num === 1 || num === "1") return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
+
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(-25);
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
