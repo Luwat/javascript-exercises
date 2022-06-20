3914c850b47ed85ed2238a8bba2338d2df6357c1
luwat <oluwatobioreniyi@gmail.com>
2022-06-20 23:42:11 +0100

    done with getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+const getTheTitles = function(books) {
+    return books.filter(book => book.title).map(book => book.title);
 };
 
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
