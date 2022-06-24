82c3335c3d7310978f0c8a07db43f4e282bc5422
luwat <oluwatobioreniyi@gmail.com>
2022-06-24 08:59:34 +0100

    still struggling

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
 const findTheOldest = function() {
-
+    
 };
 
 // Do not edit below this line
